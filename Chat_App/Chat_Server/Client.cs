d38939370e939b7139c83c19a2350ab57bba4244
N4shh <nassh2718@gmail.com>
2023-11-24 23:49:31 +0200

    commit

---
+++ Chat_App/Chat_Server/Client.cs
@@ -0,0 +1,62 @@
+﻿using Chat_Server.Net.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat_Server
+{
+    class Client
+    {
+        public string Username { get; set; }
+        public Guid UID { get; set; }
+        public  TcpClient ClientSocket { get; set; }
+
+        PacketReader _packetReader {  get; set; }
+        public Client(TcpClient client)
+        {
+            ClientSocket = client;
+            UID = Guid.NewGuid();
+
+            _packetReader = new PacketReader(ClientSocket.GetStream());
+
+            var opcode = _packetReader.ReadByte();
+            Username = _packetReader.ReadMessage();
+
+            Console.WriteLine($"[{DateTime.Now}]: Client has connected with the username: {Username}");
+
+            Task.Run(() => Procces());
+        }
+
+        void Procces()
+        {
+            while(true)
+            {
+                try
+                {
+                    var opcode = _packetReader.ReadByte();
+                    switch (opcode)
+                    {
+                        case 5:
+                            var msg = _packetReader.ReadMessage();
+                            Console.WriteLine($"[{DateTime.Now}]: Message received! {msg}");
+                            Program.BroadcastMessage($"[{Username}]: {msg}");
+                            break;
+                        default: 
+                            break;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine($"[{UID.ToString()}]: Disconnected!");
+                    Program.BroadcastDisconnect(UID.ToString());
+                    ClientSocket.Close();
+                    break;
+                }
+            }
+        }
+
+    }
+}
